13e96754e6034e79c2af851e87e5a5da4d425359
AnderProgramming <158221956+AnderssonProgramming@users.noreply.github.com>
2025-07-03 13:13:49 -0500

    feat: add spring sockets react p5 project scaffolding

---
+++ src/main/java/edu/eci/arsw/spring_sockets_rt_p5/App.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.spring_sockets_rt_p5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/edu/eci/arsw/spring_sockets_rt_p5/AppTest.java
@@ -0,0 +1,20 @@
+package edu.eci.arsw.spring_sockets_rt_p5;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
